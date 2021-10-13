24800f3c17d2e269ae609a448cde59ab138a638b
kevicai <74110291+kevicai@users.noreply.github.com>
2021-10-12 23:29:37 -0400

    Added Commodity.java, ShoppingList.java and modified ShoppingListManager.java.

---
+++ src/main/Java/entities/Commodity.java
@@ -0,0 +1,4 @@
+package Java.entities;
+
+public class Commodity {
+}
--- src/main/Java/use_cases/ShoppingListManager.java
+++ src/main/Java/use_cases/ShoppingListManager.java
@@ -1,4 +1,60 @@
 package Java.use_cases;
 
+import Java.entities.Commodity;
+import Java.entities.ShoppingList;
+import Java.entities.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Represents the entire system of Customers creating ShoppingLists.
+ */
 public class ShoppingListManager {
+
+    private HashMap<User, ShoppingList> userIDToShoppingList;
+
+    /**
+     * Create a ShoppingListManager with an empty userIDToShoppingList.
+     */
+    public ShoppingListManager(){
+        userIDToShoppingList = new HashMap<User, ShoppingList>();
+    }
+
+    /**
+     * Create and add a new ShoppingList and maps it to the User in userIDToShoppingList.
+     * If user is not
+     * @param outlet name of the outlet.
+     * @param userID user ID of the User.
+     */
+    public void newShoppingList(String outlet, String userID){
+    }
+
+    /**
+     * remove the user's ShoppingList from userToShoppingLists.
+     * If user is not
+     * @param userID user ID of the User.
+     */
+    public void newShoppingList(User userID){
+    }
+
+    /**
+     * Add commodity to the user's ShoppingList under the outlet.
+     * If user is not
+     * @param outlet name of the outlet.
+     * @param userID user ID of the User.
+     * @param commodity a Commodity.
+     */
+    public void addCommodity(String outlet, String userID, Commodity commodity){
+    }
+
+    /**
+     * Remove commodity from the user's ShoppingList under the outlet.
+     * If user is not
+     * @param outlet name of the outlet.
+     * @param userID user ID of the User.
+     * @param commodity a Commodity.
+     */
+    public void removeCommodity(String outlet, String userID, Commodity commodity){
+    }
 }
