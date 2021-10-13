af341f9f3f2e3855f844f032f01ed35fda519068
sidsachdev12 <sid.sachdev@mail.utoronto.ca>
2021-10-13 16:38:32 -0400

    Customer and DelieveryMan extending User.

--- src/main/Java/entities/User.java
+++ src/main/Java/entities/User.java
@@ -1,4 +1,30 @@
 package Java.entities;
 
-public class User {
+public abstract class User {
+
+private String name;
+private int[] location;
+private int number;
+private float rating;
+private String comment;
+private String uname;
+private String password;
+
+User(String n, int[] l, int num, String user, String pass){
+    this.name = n;
+    this.location = l;
+    this.number = num;
+    this.uname = user;
+    this.password = pass;
+}
+
+abstract String getName();
+
+abstract String getUname();
+
+abstract String getPassword();
+
+abstract int getNumber();
+
+abstract int[] getLocation();
 }
