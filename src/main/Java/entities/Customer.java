84b1d03733e10621a0f325d4fc26a7b19d90bd5b
Patrick <patrick.zhou@mail.utoronto.ca>
2021-10-13 19:06:39 -0400

    new version file that delete overide methods

--- src/main/Java/entities/Customer.java
+++ src/main/Java/entities/Customer.java
@@ -7,30 +7,5 @@ public class Customer extends User{
 
     }
 
-    @Override
-    String getName() {
-        return null;
-    }
-
-
-    @Override
-    String getUname() {
-        return null;
-    }
-
-    @Override
-    String getPassword() {
-        return null;
-    }
-
-    @Override
-    int getNumber() {
-        return 0;
-    }
-
-    @Override
-    int[] getLocation() {
-        return new int[0];
-    }
 
 }
