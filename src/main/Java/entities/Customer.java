7fb65ba0258b005abe21a40c4222682c6759c395
Patrick <patrick.zhou@mail.utoronto.ca>
2021-10-13 23:44:04 -0400

    rewrite Override methods

--- src/main/Java/entities/Customer.java
+++ src/main/Java/entities/Customer.java
@@ -2,10 +2,17 @@ package entities;
 
 public class Customer extends User{
 
-    Customer(String n, int[] l, int num, String user, String pass){
+    public Customer(String n, int[] l, int num, String user, String pass){
         super(n, l, num, user, pass);
 
     }
 
-
+    @Override
+    public String getUname() {
+        return super.getUname();
+    }
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
 }
