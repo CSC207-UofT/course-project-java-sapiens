1975bf851f304ab60b6cc6890e7ea17b7afb07ef
kevicai <74110291+kevicai@users.noreply.github.com>
2021-11-01 14:55:02 -0400

    Refactored ShoppingList and ShoppingListManager to Old, inorder to implement them in another way as suggested by TA in TUT and phase 0.

--- src/test/java/entities/ShoppingListTest.java
+++ src/test/java/entities/ShoppingListTest.java
@@ -6,28 +6,28 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ShoppingListTest {
-    ShoppingList shoppingList;
+    ShoppingListOld shoppingListOld;
     @Before
     public void setUp() throws Exception {
-        shoppingList = new ShoppingList();
+        shoppingListOld = new ShoppingListOld();
     }
 
     @Test
     public void addCommodity() {
         Commodity apple = new Commodity("Apple", 3.5);
-        shoppingList.addCommodity("Walmart", apple);
-        assertEquals( 3.5, shoppingList.getTotalPrice(), 0);
+        shoppingListOld.addCommodity("Walmart", apple);
+        assertEquals( 3.5, shoppingListOld.getTotalPrice(), 0);
     }
 
     @Test
     public void removeCommodity() {
         Commodity apple = new Commodity("Apple", 3.5);
-        shoppingList.addCommodity("Walmart", apple);
-        shoppingList.addCommodity("Walmart", apple);
-        shoppingList.removeCommodity("Walmart", apple);
-        assertEquals( 3.5, shoppingList.getTotalPrice(), 0);
-        shoppingList.removeCommodity("Walmart", apple);
-        assertEquals( 0, shoppingList.getTotalPrice(), 0);
+        shoppingListOld.addCommodity("Walmart", apple);
+        shoppingListOld.addCommodity("Walmart", apple);
+        shoppingListOld.removeCommodity("Walmart", apple);
+        assertEquals( 3.5, shoppingListOld.getTotalPrice(), 0);
+        shoppingListOld.removeCommodity("Walmart", apple);
+        assertEquals( 0, shoppingListOld.getTotalPrice(), 0);
 
     }
 }
