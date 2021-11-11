72debfea66dd87cbfa6ff7135233581d5ae8dd88
kevicai <74110291+kevicai@users.noreply.github.com>
2021-11-11 16:20:20 -0500

    Created TODOs in CustomerActivity

--- src/main/java/ui/CustomerActivity.java
+++ src/main/java/ui/CustomerActivity.java
@@ -1,15 +1,24 @@
 package ui;
 
+import controllers.use_cases.CustomerManager;
+import controllers.use_cases.ShoppingListManager;
+import entities.Customer;
+import entities.ShoppingList;
+
 import java.util.ArrayList;
 
 public class CustomerActivity implements Activity{
+    private Customer cus;
     @Override
     public void display() {
+        // TO-DO Create a shopping list
+        sio.intent(new ShoppingListActivity(), null);
+        // TO-DO Check order status
 
     }
 
     @Override
     public void getData(ArrayList<Object> transferredData) {
-
+        this.cus = (Customer) transferredData.get(0);
     }
 }
---
+++ src/main/java/ui/ShoppingListActivity.java
@@ -0,0 +1,16 @@
+package ui;
+
+import entities.Customer;
+
+import java.util.ArrayList;
+
+public class ShoppingListActivity implements Activity{
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void getData(ArrayList<Object> transferredData) {
+    }
+}
