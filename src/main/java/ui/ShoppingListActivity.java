2b84530fd8280db24a8e85be46f38886e0344068
kevicai <74110291+kevicai@users.noreply.github.com>
2021-11-11 20:04:22 -0500

    Resolved merge conflicts in ShoppingListActivity

--- src/main/java/ui/ShoppingListActivity.java
+++ src/main/java/ui/ShoppingListActivity.java
@@ -88,9 +88,8 @@ public class ShoppingListActivity implements Activity{
     }
 
     @Override
-
-    public void getData(ArrayList<Object> transferredData) {
-        this.customer = (Customer) transferredData.get(0);
+    public void getData(Object transferredData) {
+        this.customer = (Customer) transferredData;
     }
 }
 
