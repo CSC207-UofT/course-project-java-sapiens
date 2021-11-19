39abf5ca12904975440966e6123a6314aa426031
yde773786 <yde773786@gmail.com>
2021-11-19 18:35:13 -0500

    Android template added

---
+++ app/src/main/java/com/yde/sapiensdelivery/UI/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.yde.sapiensdelivery.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.yde.sapiensdelivery.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
