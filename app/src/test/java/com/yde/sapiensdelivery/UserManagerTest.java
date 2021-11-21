a3008fbbd2afcbc93351107e826d065085e6fa07
yde773786 <yde773786@gmail.com>
2021-11-21 04:47:00 -0500

    Confirmed functionality of Sign In

---
+++ app/src/test/java/com/yde/sapiensdelivery/UserManagerTest.java
@@ -0,0 +1,47 @@
+package com.yde.sapiensdelivery;
+
+import com.yde.sapiensdelivery.controllers.CustomerManager;
+import com.yde.sapiensdelivery.controllers.database.OnDataReadListener;
+import com.yde.sapiensdelivery.entities.Customer;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserManagerTest {
+    CustomerManager cm;
+
+    @Before
+    public void setUp() throws InterruptedException {
+        cm = new CustomerManager("CUSTOMER");
+
+        Customer kevin = new Customer("Kevin", new int[] {100, 200}, "4164449999", "kevin",
+                DigestUtils.sha256Hex("ilikesparrows123"));
+
+        cm.save(kevin.getUname(), kevin);
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testRead() throws InterruptedException {
+        final String[] assertResult = {""};
+
+        cm.get("Kevin", new OnDataReadListener() {
+            @Override
+            public void onSuccess() {
+                Customer customer = (Customer) getSavedObject();
+                assertResult[0] = customer.getName();
+            }
+
+            @Override
+            public void onFailure() {
+                System.out.println("Failure of database.");
+            }
+        });
+        Thread.sleep(10000);
+        assertEquals("Kevin", assertResult[0]);
+    }
+
+}
