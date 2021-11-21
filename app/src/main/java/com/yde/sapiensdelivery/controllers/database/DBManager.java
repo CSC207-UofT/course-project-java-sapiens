3a27806305b1af877e3e3485592353d521852617
yde773786 <yde773786@gmail.com>
2021-11-21 02:45:11 -0500

    User Management classes added

---
+++ app/src/main/java/com/yde/sapiensdelivery/controllers/database/DBManager.java
@@ -0,0 +1,32 @@
+package com.yde.sapiensdelivery.controllers.database;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public abstract class DBManager<K, V> {
+
+    public FirebaseDatabase database;
+    public DatabaseReference ref;
+
+    public DBManager(){
+        try {
+            database = FirebaseDatabase.getInstance();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * All Manager classes in controllers.use_cases have some transactions to save.
+     * @param obj The 'key' with which the database can be queried
+     * @param val The corresponding object
+     */
+    abstract public void save(K obj, V val);
+
+    /**
+     * All Manager classes query the database for a specific object type that it is managing.
+     * @param obj The 'key' with which the database can be queried.
+     */
+    abstract public void get(K obj, final OnDataReadListener onDataReadListener);
+
+}
