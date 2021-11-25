a762364c116c0c4a5d05d593fdfb7fefe34205bb
yde773786 <yde773786@gmail.com>
2021-11-25 14:57:56 -0500

    RadioButton click hides/reveals

--- app/src/main/java/com/yde/sapiensdelivery/ui/RegistrationActivity.java
+++ app/src/main/java/com/yde/sapiensdelivery/ui/RegistrationActivity.java
@@ -3,6 +3,9 @@ package com.yde.sapiensdelivery.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 import com.yde.sapiensdelivery.R;
 
@@ -12,5 +15,22 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        RadioGroup userType = findViewById(R.id.user_type);
+        LinearLayout[] optionalLLs = {findViewById(R.id.rate_ll), findViewById(R.id.sin_ll), findViewById(R.id.transport_ll)};
+
+        userType.setOnCheckedChangeListener((radioGroup, i) -> {
+            if(i == R.id.customer_sign_up){
+                for(LinearLayout optionLL : optionalLLs){
+                    optionLL.setVisibility(View.GONE);
+                }
+            }
+            else{
+                for(LinearLayout optionLL : optionalLLs){
+                    optionLL.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }
