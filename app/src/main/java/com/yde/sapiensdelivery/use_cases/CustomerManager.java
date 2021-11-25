9ddf9201ca408584e5b7f8f9bd816631d14f38f3
kevicai <74110291+kevicai@users.noreply.github.com>
2021-11-25 14:42:54 -0500

    Added methods in use cases

--- app/src/main/java/com/yde/sapiensdelivery/use_cases/CustomerManager.java
+++ app/src/main/java/com/yde/sapiensdelivery/use_cases/CustomerManager.java
@@ -4,6 +4,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.yde.sapiensdelivery.controllers.database.OnDataReadListener;
+import com.yde.sapiensdelivery.entities.Order;
 import com.yde.sapiensdelivery.entities.User;
 import com.yde.sapiensdelivery.entities.Customer;
 import com.yde.sapiensdelivery.entities.User;
@@ -14,87 +15,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerManager extends UserManager {
-
-//    final String REF_PATH = "Customer";
-//
-//    public CustomerManager(String userType) {
-//        super(userType);
-//        ref = database.getReference(REF_PATH);
-//    }
-//
-//    @Override
-//    protected boolean discrepancyCheck(User currUser) {
-//        return false;
-//    }
-//
-//    /**
-//     * Returns the user if existing in database
-//     *
-//     * @param uname              Username of user
-//     * @param password           Password of user
-//     * @param onDataReadListener  Action to be performed on authentication
-//     */
-//    @Override
-//    public void authenticate(String uname, String password, final OnDataReadListener onDataReadListener) {
-//
-//        ref.child(uname).addListenerForSingleValueEvent(new ValueEventListener() {
-//
-//            @Override
-//            public void onDataChange(DataSnapshot snapshot) {
-//                Customer customer = snapshot.getValue(Customer.class);
-//
-//                if(customer == null){
-//                    onDataReadListener.onFailure();
-//                }
-//                else if(customer.getPassword().equals(createHash(password))){
-//                    onDataReadListener.setSavedObject(snapshot.getValue(Customer.class));
-//                    onDataReadListener.onSuccess();
-//                }
-//                else{
-//                    onDataReadListener.onFailure();
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                onDataReadListener.onFailure();
-//            }
-//        });
-//    }
+    private Customer customer;
 
     /**
-     * All Manager classes in controllers.use_cases have some transactions to save.
-     *
-     * @param obj The username with which the database can be queried
-     * @param val The corresponding Customer
+     * Creates an OrderManager given an already existing Customer
      */
-//    @Override
-    public void save(String obj, User val) {
-//        Map<String, Customer> toSave = new HashMap<>();
-//        toSave.put(obj, (Customer) val);
-//
-//        ref.setValueAsync(toSave);
+    public CustomerManager(Customer customer) {
+        this.customer = customer;
     }
 
     /**
-     * All Manager classes query the database for a specific object type that it is managing.
-     *
-     * @param obj The username with which the database can be queried.
+     * Creates an OrderManager
      */
-//    @Override
-    public void get(String obj, final OnDataReadListener onDataReadListener){
-//        ref.child(obj).addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot snapshot) {
-//                onDataReadListener.setSavedObject(snapshot.getValue(Customer.class));
-//                onDataReadListener.onSuccess();
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError error) {
-//                onDataReadListener.onFailure();
-//            }
-//        });
+    public CustomerManager() {
     }
+
 
 }
