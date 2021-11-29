e0c04df7e30c16d23829d21cdf9bad6305ecca53
PatrickZhou17 <44577726+PatrickZhou17@users.noreply.github.com>
2021-11-29 13:44:37 -0500

    Create OrderManagerTest file

--- app/src/test/java/use_cases_tests/OrderManagerTest.java
+++ app/src/test/java/use_cases_tests/OrderManagerTest.java
@@ -18,7 +18,7 @@ public class OrderManagerTest {
     @Test(timeout = 50)
     public void testCreateOrder1() {
         DeliveryMan deliveryMan1 = new DeliveryMan("Samuel", new int[] {10,20}, "648", "Samuel", "12", 1234, "moto-bike", (float)4.5);
-        Customer customer1 = new Customer("Patrick", new int[] {4,20}, "647", "Pat", "123");
+        Customer customer1 = new Customer("Patrick", new int[] {4,29}, "647", "Pat", "123");
         ShoppingList shoppingList = new ShoppingList("KFC");
         ArrayList<ShoppingList> s = new ArrayList<>();
         s.add(shoppingList);
@@ -36,6 +36,6 @@ public class OrderManagerTest {
         s.add(shoppingList);
         Order order = orderManager.createOrder(deliveryMan1, customer1, s);
 
-        assertEquals("648", order.getDeliveryMan().getNumber());
+        assertEquals("647", order.getDeliveryMan().getNumber());
     }
 }
