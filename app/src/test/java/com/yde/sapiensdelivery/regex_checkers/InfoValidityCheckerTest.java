34524812da486a7d93e9624b685d7a53bda38737
kevicai <74110291+kevicai@users.noreply.github.com>
2021-11-29 11:50:30 -0500

    Fixed error warnings.

--- app/src/test/java/com/yde/sapiensdelivery/regex_checkers/InfoValidityCheckerTest.java
+++ app/src/test/java/com/yde/sapiensdelivery/regex_checkers/InfoValidityCheckerTest.java
@@ -1,17 +1,16 @@
 package com.yde.sapiensdelivery.regex_checkers;
 
-import com.yde.sapiensdelivery.gateways.DeliveryManGateway;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class InfoValidityCheckerTest {
     @Test
     public void isDeliveryManInfoInvalidTrue() {
-        HashMap<String, String> fieldToValue = new HashMap<String, String>();
+        HashMap<String, String> fieldToValue = new HashMap<>();
         fieldToValue.put("PHONE NUMBER", "1231231234");
         fieldToValue.put("SIN", "123456789");
         fieldToValue.put("TRANSPORT", "MOM5CAR");
@@ -20,7 +19,7 @@ public class InfoValidityCheckerTest {
 
     @Test
     public void isDeliveryManInfoInvalidFalse() {
-        HashMap<String, String> fieldToValue = new HashMap<String, String>();
+        HashMap<String, String> fieldToValue = new HashMap<>();
         fieldToValue.put("PHONE NUMBER", "123");
         fieldToValue.put("SIN", "12345678910");
         fieldToValue.put("TRANSPORT", "M");
