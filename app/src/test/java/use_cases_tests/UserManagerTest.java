906b210f2bad6235c0a81b24f4c69ecfe9a42c2c
Patrick <patrick.zhou@mail.utoronto.ca>
2021-11-29 13:58:46 -0500

    create UserManagerTest file

---
+++ app/src/test/java/use_cases_tests/UserManagerTest.java
@@ -0,0 +1,12 @@
+package use_cases_tests;
+
+import com.yde.sapiensdelivery.entities.Customer;
+import com.yde.sapiensdelivery.use_cases.CustomerManager;
+import com.yde.sapiensdelivery.use_cases.UserManager;
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class UserManagerTest {
+    UserManager userManager = new UserManager();
+
+}
