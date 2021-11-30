1f4b44b16f9d36435bca05974ddd355d0ced9701
Patrick <patrick.zhou@mail.utoronto.ca>
2021-11-29 22:45:08 -0500

    update CustomerActivity

--- app/src/main/java/com/yde/sapiensdelivery/controllers/CustomerActivity.java
+++ app/src/main/java/com/yde/sapiensdelivery/controllers/CustomerActivity.java
@@ -1,15 +1,49 @@
 package com.yde.sapiensdelivery.controllers;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yde.sapiensdelivery.R;
 
 public class CustomerActivity extends AppCompatActivity {
-
+    private Button button1;
+    private Button button2;
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        button1 = (Button) findViewById(R.id.profile);
+        button2 = (Button) findViewById(R.id.status);
+        button3 = (Button) findViewById(R.id.placeOrder);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+//                Intent intent = new Intent( CustomerActivity.this, ProfileActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+//                Intent intent = new Intent( CustomerActivity.this, OrderStatusActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+//                Intent intent = new Intent( CustomerActivity.this, OrderCreationActivity.class);
+//                startActivity(intent);
+            }
+        });
     }
 }
