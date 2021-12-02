a960cde237807f4812b4278e7d4d2827ad0f9256
kevicai <74110291+kevicai@users.noreply.github.com>
2021-12-02 15:30:21 -0500

    Deleted a new type of Commodity that we don't have time to implement.

--- app/src/main/java/com/yde/sapiensdelivery/entities/Notable.java
+++
@@ -1,8 +0,0 @@
-package com.yde.sapiensdelivery.entities;
-
-public interface Notable {
-        void markEasyToBreak ();
-        void markPet ();
-        void setNote(String note);
-        String getNote();
-}
