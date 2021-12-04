e4846991e4a04b8f2e5c37db7f8948552c12a20f
Junsong Guo <469065047@qq.com>
2021-12-03 21:37:16 -0500

    Completed parsing of the JSON file in GoogleMapGateway.java

--- app/src/main/java/com/yde/sapiensdelivery/gateways/GoogleMapGateway.java
+++ app/src/main/java/com/yde/sapiensdelivery/gateways/GoogleMapGateway.java
@@ -1,25 +1,5 @@
 package com.yde.sapiensdelivery.gateways;
 
-//import java.io.IOException;
-//import java.net.URL;
-//import java.net.URLConnection;
-//import java.util.Scanner;
-//
-//import org.json.JSONException;
-//
-//public class GoogleMapGateway {
-//
-//    private final String apikey = "AIzaSyAxeqdWPsIhW7KXVSef1uH0OmAX8Pnqb2M";
-//
-//    public static void main(String[] args) throws IOException{
-//        URLConnection connection = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyAxeqdWPsIhW7KXVSef1uH0OmAX8Pnqb2M").openConnection();
-//        try(Scanner scanner = new Scanner(connection.getInputStream());){
-//            String response = scanner.useDelimiter("\\A").next();
-//            System.out.println(response);
-//        }
-//    }
-//}
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,6 +7,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import org.json.JSONException;
@@ -34,7 +16,36 @@ import org.json.JSONObject;
 
 public class GoogleMapGateway {
 
-    private static String readAll(Reader rd) throws IOException {
+
+    public GoogleMapGateway() {
+    }
+
+    public HashMap<String, String> findRouteInfo(String customerLocation,
+                                                 String deliverymanLocation)
+                                                 throws IOException, JSONException {
+
+        HashMap<String, String> routeInfo = new HashMap<String, String>();
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" +
+                customerLocation +
+                "&destination=" +
+                deliverymanLocation +
+                "&key=AIzaSyAxeqdWPsIhW7KXVSef1uH0OmAX8Pnqb2M";
+
+        JSONObject json = this.readJsonFromUrl(url);
+
+        JSONObject info = json.getJSONArray("routes").
+                getJSONObject(0).
+                getJSONArray("legs").
+                getJSONObject(0);
+
+        String duration = info.getJSONObject("duration").getString("text");
+        String distance = info.getJSONObject("distance").getString("text");
+        routeInfo.put("Duation", duration);
+        routeInfo.put("Distance", distance);
+        return routeInfo;
+    }
+
+    private String readAll(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
         int cp;
         while ((cp = rd.read()) != -1) {
@@ -43,30 +54,20 @@ public class GoogleMapGateway {
         return sb.toString();
     }
 
-    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+    private JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
 
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            System.out.println(jsonText);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
+        try (InputStream is = new URL(url).openStream()) {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = this.readAll(rd);
+            return new JSONObject(jsonText);
         }
     }
 
-    public static void main(String[] args) throws IOException, JSONException {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the JSON URL :");
-        String url = in.next();
-        JSONObject json = readJsonFromUrl(url);
-        int op1= Integer.parseInt((String)json.get("Operand1"));
-        int op2= Integer.parseInt((String)json.get("Operand2"));
-        int result = op1+op2;
-        System.out.println("Result is : "+ result);
-    }
-
+//    public static void main(String[] args) throws IOException, JSONException {
+//        GoogleMapGateway gmg = new GoogleMapGateway();
+//        HashMap<String,String> routeInfo = gmg.findRouteInfo("DundasWest",
+//                "UniversityofToronto");
+//        System.out.println(routeInfo);
+//    }
 
 }
