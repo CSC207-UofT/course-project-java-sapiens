0ee76e44db5d2e92a61bc73362a851f40733c944
Patrick Zhou <patrickzhou17@hotmail.com>
2021-12-04 00:53:01 -0500

    rename the profile activity to CProfileActivity, and use gateway methods to get the information of data

--- app/src/main/java/com/yde/sapiensdelivery/controllers/CProfileActivity.java
+++ app/src/main/java/com/yde/sapiensdelivery/controllers/CProfileActivity.java
@@ -9,19 +9,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.yde.sapiensdelivery.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.yde.sapiensdelivery.entities.Customer;
 import com.yde.sapiensdelivery.gateways.UserGateway;
-import com.yde.sapiensdelivery.gateways.database.OnDataReadListener;
+import com.yde.sapiensdelivery.use_cases.CustomerManager;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.ValueEventListener;
-import com.yde.sapiensdelivery.gateways.database.OnDataReadListener;
-
-import java.util.concurrent.atomic.AtomicMarkableReference;
-
-public class ProfileActivity extends AppCompatActivity {
+public class CProfileActivity extends AppCompatActivity {
     private Button main;
+    private FirebaseDatabase user;
+    private DatabaseReference ref;
 
+    UserGateway userGateway;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +33,21 @@ public class ProfileActivity extends AppCompatActivity {
         main.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( ProfileActivity.this, CustomerActivity.class);
+                Intent intent = new Intent( CProfileActivity.this, CustomerActivity.class);
                 startActivity(intent);
             }
         });
+
+        CustomerManager cm = new CustomerManager((Customer) getIntent().getSerializableExtra("CUSTOMER"));
 
         TextView fullName = (TextView) findViewById(R.id.FullName);
         TextView userName = (TextView) findViewById(R.id.UserName);
         TextView phone = (TextView) findViewById(R.id.phone);
 
 
-
-        String Fullname = "";
-        String Username = "";
-        String Phone  = "";
+        String Fullname = cm.getName();
+        String Username = cm.getUsername();
+        String Phone  = cm.getPhoneNumber();
         fullName.setText(Fullname);
         userName.setText(Username);
         phone.setText(Integer.parseInt(Phone));
