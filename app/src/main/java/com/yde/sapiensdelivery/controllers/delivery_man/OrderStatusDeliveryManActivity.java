3bbbf79afb83da411212d7fac640ed9218a43a50
Junsong Guo <469065047@qq.com>
2021-12-05 17:49:19 -0500

    OrderStatusDeliveryManActivity added, along with its layout file

---
+++ app/src/main/java/com/yde/sapiensdelivery/controllers/delivery_man/OrderStatusDeliveryManActivity.java
@@ -0,0 +1,14 @@
+package com.yde.sapiensdelivery.controllers.delivery_man;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderStatusDeliveryManActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_status_delivery_man);
+    }
+}
