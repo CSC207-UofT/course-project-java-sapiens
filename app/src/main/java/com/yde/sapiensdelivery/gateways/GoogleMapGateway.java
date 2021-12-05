7ea518578dec576df43f779b942c9cd4a0957c50
Junsong Guo <469065047@qq.com>
2021-12-05 00:10:30 -0500

    Implemented findCurrentLocation method. Now can return the current location

--- app/src/main/java/com/yde/sapiensdelivery/gateways/GoogleMapGateway.java
+++ app/src/main/java/com/yde/sapiensdelivery/gateways/GoogleMapGateway.java
@@ -1,8 +1,19 @@
 package com.yde.sapiensdelivery.gateways;
 
-//import com.google.android.gms.location.FusedLocationProviderClient;
-//import com.google.android.gms.location.LocationServices;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
+import androidx.core.app.ActivityCompat;
+
 import com.yde.sapiensdelivery.use_cases.Locator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,11 +21,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-        import java.nio.charset.StandardCharsets;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-
-        import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * A gateway method that gets data from the GoogleMap Direction API
@@ -22,16 +30,34 @@ import org.json.JSONObject;
  * Implements the Locator Interface.
  */
 public class GoogleMapGateway implements Locator {
-    //    private FusedLocationProviderClient fusedLocationClient;
 
     public GoogleMapGateway() {
     }
 
     // Private enum for different types of information
-    private enum infoType{
-        duration, distance, endAddress, startAddress
+    private enum infoType {
+        duration, distance
     }
 
+    public String findCurrentLocation(Activity activity) {
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(activity, new String[] {
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                }, 1);
+//            }
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        return lat+","+lng;
+    }
 
 
     /**
